da1dc46940d2ff83b2e0cb6a9e3846b573ae9520
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-01 19:25:25 +0530

    added first instruction

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This function is a simple 3-function non-custodial escrow that operates on-chain
 - create a new escrow linked to the assets they wish to exchange for a new one.
 - accept the escrow and exchange the current assets for a new one.
 - if they don't wish to exchange their assets for new ones, they can cancel the escrow
+
+Our first instruction, "initialize" will create a new escrow account associated with our old token account for a new one. For this program, we are going to sell our 'x_token' for 'y_token'. In order to make this program non-custodial, we will first transfer our 'x_token' to the program's owned 'escrowed_x_tokens' accounts.
--- programs/non-custodial-escrow/src/lib.rs
+++ programs/non-custodial-escrow/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("ABhLzTy8jQMMPb7P1PbdggUFpSNn61XGrhnRRsVSRzAi");
 
@@ -6,10 +7,68 @@ declare_id!("ABhLzTy8jQMMPb7P1PbdggUFpSNn61XGrhnRRsVSRzAi");
 pub mod non_custodial_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, x_amount: u64, y_amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.bump = *ctx.bumps.get("escrow").unwrap();
+        escrow.authority = ctx.accounts.seller.key();
+        escrow.escrowed_x_tokens = ctx.accounts.escrowed_x_tokens.key();
+        escrow.y_amount = y_amount; // number of tokens seller wants to recieve
+        escrow.y_mint = ctx.accounts.y_mint.key(); // mint of the token, seller wants to recieve
+
+        // transfer seller's x_token into program owned escrow token account
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.seller_x_token.to_account_info(),
+                    to: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                    authority: ctx.accounts.seller.to_account_info(),
+                },
+            ),
+            x_amount,
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    seller: Signer<'info>,
+    x_mint: Account<'info, Mint>,
+    y_mint: Account<'info, Mint>,
+    #[account(mut, constraint = seller_x_token.mint == x_mint.key() && seller_x_token.owner == seller.key())]
+    seller_x_token: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = seller,
+        space = Escrow::LEN,
+        seeds = ["escrow".as_bytes(), seller.key().as_ref()],
+        bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        init,
+        payer = seller,
+        token::mint = y_mint,
+        token::authority = escrow,
+    )]
+    escrowed_x_tokens: Account<'info, TokenAccount>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Escrow {
+    authority: Pubkey,
+    bump: u8,
+    escrowed_x_tokens: Pubkey,
+    y_mint: Pubkey,
+    y_amount: u64,
+}
+
+impl Escrow {
+    const LEN: usize = 8 + 1 + 32 + 32 + 32 + 8;
+}
